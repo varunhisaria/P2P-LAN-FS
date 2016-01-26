aa591cdccae4edc08131b1a8575fb463c2b2d894
Palash Chatterjee <palash@protonmail.ch>
2016-01-26 12:25:11 +0530

    Error handling

--- src/connectionManager.cpp
+++ src/connectionManager.cpp
@@ -9,6 +9,7 @@
 #define LOCALHOST "127.0.0.1"
 #define LISTENINGPORT 9090
 #define DEBUG true
+#define BLOCK_SIZE 4096
 
 using namespace std;
 
@@ -25,6 +26,8 @@ class ConnectionManager{
 		string self_ip_address;
 		string sending_ip_address;
 		unsigned int number_of_connections;
+		char* buffer;
+		char* data;
 		
 	public:
 		// Constructor
@@ -44,10 +47,32 @@ class ConnectionManager{
 
 		// Initialise a TCP or UDP socket based on the boolean is_tcp
 		void init(){
-			if(this->is_tcp)
-				this->socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
-			else
-				this->socket_descriptor = socket(AF_INET, SOCK_DGRAM, 0);
+			if(this->is_tcp){
+				if((this->socket_descriptor = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+					if(DEBUG){
+						cout<<"Error while creating TCP socket\n";
+						cout<<strerror(errno);
+					}
+					exit(1);
+				}
+			}
+			else{
+				if((this->socket_descriptor = socket(AF_INET, SOCK_DGRAM, 0)==-1){
+					if(DEBUG){
+						cout<<"Error while creating UDP socket\n";
+						cout<<strerror(errno);
+					}
+					exit(1);
+				}
+			}
+
+			if(setsockopt(this->socket_descriptor, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int))==-1){
+				if(DEBUG){
+					cout<<"Error while setting socket options.\n";
+					cout<<strerror(errno);
+				}
+				exit(1);
+			}
 		}
 
 		// Start a server. TCP or UDP dependent on the socket type.
@@ -61,29 +86,31 @@ class ConnectionManager{
 			server.sin_addr.s_addr=inet_addr(this->self_ip_address.c_str());
 			server.sin_port = htons(this->listening_port);
 
-			// Error handling to be done.
+			// Bind socket to address
 			if(bind(this->socket_descriptor, (struct sockaddr*)&server, sizeof(server))!=0){
 				if(DEBUG){
-					cout<<"Error while binding socket to local address."<<endl;
-					cout<<"Origin : ConnectionManager::startServer()"<<endl;
-					cout<<errno;
+					cout<<"Error while binding socket to local address.\n";
+					cout<<"Origin : ConnectionManager::startServer()\n";
 					cout<<strerror(errno);
 				}
+				exit(1);
 			}
 
+			// Listen for connections 
 			if(listen(this->socket_descriptor, this->number_of_connections)!=0){
 					if(DEBUG){
-						cout<<"Error while listening for connections."<<endl;
-						cout<<"Origin : ConnectionManager::startServer()"<<endl;
+						cout<<"Error while listening for connections.\n"
+						cout<<"Origin : ConnectionManager::startServer()\n";
 						cout<<strerror(errno);
 					}
+					exit(1);
 				}
 
 			// Debugging information
 			if(DEBUG){
-				cout<<"Server started. Waiting for connections.";
-				cout<<"Listening on port :: "<<this->listening_port;
-				cout<<"Number of connections allowed :: "<<this->number_of_connections;
+				cout<<"Server started. Waiting for connections.\n";
+				cout<<"Listening on port :: "<<this->listening_port<<endl;
+				cout<<"Number of connections allowed :: "<<this->number_of_connections<<endl;
 			}
 
 			fflush(stdout);
@@ -112,12 +139,24 @@ class ConnectionManager{
 			server.sin_port = htons(this->sending_port);
 
 			if(DEBUG){
-				cout<<"Attempting to connect to "<<this->sending_ip_address<<" at port "<<this->sending_port;
-				cout<<"Connection going from "<<this->self_ip_address<<" and port "<<this->listening_port;
+				cout<<"Attempting to connect to "<<this->sending_ip_address<<" at port "<<this->sending_port<<endl;
+				cout<<"Connection going from "<<this->self_ip_address<<" and port "<<this->listening_port<<endl;
 			}
 			fflush(stdout);
 			// Connect to the server. Error handling to be done.
-			connect(socket_descriptor, (struct sockaddr*)&server, sizeof(server));
+			connect(this->socket_descriptor, (struct sockaddr*)&server, sizeof(server));
+		}
+
+		void sendDataToServer(string data){
+			this->data = data.c_str();
+			send(this->socket_descriptor, this->data, strlen(this->data),0);
+			if(recv(this->socket_descriptor, buffer, sizeof(buffer),0)==0){
+				if(DEBUG)
+					cout<<"Connection terminated by server.\n";
+			}
+			if(DEBUG){
+				cout<<"Server Responded : "<<buffer<<endl;
+			}
 		}
 
 		void getServerDetails(){
