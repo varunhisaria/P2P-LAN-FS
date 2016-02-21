87e6ce47e201b89f6e29e02762b64bac7c7433a3
Varun Kumar <varunhisaria@gmail.com>
2016-02-21 15:57:32 +0530

    added meta file creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include "utils.h"
+#include "metadata.h"
 
 using namespace std;
+
+//test methods
 
 void getFilesInDirectoryTest()
 {
@@ -11,9 +14,52 @@ void getFilesInDirectoryTest()
 		cout<<res[i]<<endl;
 }
 
+void getMD5Test()
+{
+	Utils obj;
+	string res = obj.getMD5("hello");
+	cout<<res<<endl;
+}
+
+void writeMetaDataTest()
+{
+    Utils obj;
+    obj.writeMetaData("main.cpp");
+}
+
+void readMetaDataTest()
+{
+    Utils obj;
+    MetaData metaData = obj.readMetaData("main.cpp");
+    cout << metaData.fileName << '\n';
+    cout << metaData.fileSize << '\n';
+    cout << metaData.fileHash << '\n';
+    cout << metaData.lastUpdated << '\n';
+    cout << metaData.noOfBlocks << '\n';
+    for(int i = 0; i<metaData.noOfBlocks; i++)
+    {
+        cout << metaData.blockHash[i];
+        if(i!=metaData.noOfBlocks-1)
+            cout<<'\n';
+    }
+}
+
+
+//actual main methods
+void addFile(string fileName)
+{
+    Utils utils;
+    utils.writeMetaData(fileName);
+    //update list file
+}
+
+
 int main()
 {
-	getFilesInDirectoryTest();
-	return 0;	
+	//getFilesInDirectoryTest();
+	//getMD5Test();
+	writeMetaDataTest();
+	readMetaDataTest();
+	return 0;
 }
 
--- src/utils.h
+++ src/utils.h
@@ -2,14 +2,21 @@
 #define __UTILS_H_INCLUDED__
 
 #include <vector>
+#include "metadata.h"
 
 using namespace std;
 
-class Utils 
+class Utils
 {
 	public:
 	vector<string> getFilesInDirectory();
-
+	pair<long long int,long long int> getSizeAndNoOfBlocks(string fileName);
+	string getMD5(string text);
+	vector<string> getMD5OfAllBlocks(string filename);
+	string getMD5OfFile(string filename);
+	void writeMetaData(string fileName);
+	MetaData readMetaData(string fileName);
+	void updateFilesList();
 };
 
 #endif
